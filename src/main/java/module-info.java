d7145e289372091653e5b9d974e7c29530a77775
Phileas <phileas.strassl@stud.fh-campus.ac.at>
2023-12-29 19:46:43 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.javaprojekt.finalversionjavaproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires kotlin.stdlib;
+
+    opens com.javaprojekt.finalversionjavaproject to javafx.fxml;
+    exports com.javaprojekt.finalversionjavaproject;
+}
